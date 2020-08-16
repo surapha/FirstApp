662ad66a8dcda727a9fe1b4b2dc29d6b63e659c9
USER PC <surapha.nu@rmuti.ac.th>
2020-08-16 22:26:33 +0700

    create class WordRoomDatabase

--- app/src/main/java/android/example/com/firstapp/database/WordRoomDatabase.kt
+++ app/src/main/java/android/example/com/firstapp/database/WordRoomDatabase.kt
@@ -1,4 +1,35 @@
 package android.example.com.firstapp.database
 
-class WordRoomDatabase {
-}
+import android.content.Context
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): WordRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                )
+                    // Wipes and rebuilds instead of migrating if no Migration object.
+                    // Migration is not part of this codelab.
+                    .fallbackToDestructiveMigration()
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
