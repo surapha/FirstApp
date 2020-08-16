6f93aee4dbe3503c6f2d1f6fbae817e58ff55e59
USER PC <surapha.nu@rmuti.ac.th>
2020-08-16 22:25:11 +0700

    create class WordRepository

--- app/src/main/java/android/example/com/firstapp/database/WordRepository.kt
+++ app/src/main/java/android/example/com/firstapp/database/WordRepository.kt
@@ -1,4 +1,17 @@
 package android.example.com.firstapp.database
 
-class WordRepository {
+import android.arch.lifecycle.LiveData
+import android.support.annotation.WorkerThread
+
+class WordRepository(private val wordDao: WordDao) {
+
+
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
 }
