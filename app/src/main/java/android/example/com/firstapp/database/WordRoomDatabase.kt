b733608e13a79ece892592179e038e038a234cda
USER PC <surapha.nu@rmuti.ac.th>
2020-08-16 22:29:20 +0700

    Update fun populateDatabase

--- app/src/main/java/android/example/com/firstapp/database/WordRoomDatabase.kt
+++ app/src/main/java/android/example/com/firstapp/database/WordRoomDatabase.kt
@@ -51,3 +51,16 @@ abstract class WordRoomDatabase : RoomDatabase() {
                 }
             }
         }
+        fun populateDatabase(wordDao: WordDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            wordDao.deleteAll()
+
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+        }
+    }
+
+}
