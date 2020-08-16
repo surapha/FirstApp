f81ce8b86e5d83a8afa44854f1085cf8a02c47c8
USER PC <surapha.nu@rmuti.ac.th>
2020-08-16 22:21:23 +0700

    create class WordDao

--- app/src/main/java/android/example/com/firstapp/database/WordDao.kt
+++ app/src/main/java/android/example/com/firstapp/database/WordDao.kt
@@ -1,4 +1,15 @@
 package android.example.com.firstapp.database
 
+@Dao
 interface WordDao {
+
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
 }
