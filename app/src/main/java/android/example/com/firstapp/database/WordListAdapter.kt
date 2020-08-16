1a148717fbc2da607bbd8d81da79e4a033a04978
USER PC <surapha.nu@rmuti.ac.th>
2020-08-16 22:23:10 +0700

    create class WordListAdapter

--- app/src/main/java/android/example/com/firstapp/database/WordListAdapter.kt
+++ app/src/main/java/android/example/com/firstapp/database/WordListAdapter.kt
@@ -1,4 +1,10 @@
 package android.example.com.firstapp.database
 
-class WordListAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+
+
+class WordListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
 }
